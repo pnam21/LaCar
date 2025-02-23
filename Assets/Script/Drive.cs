7ea5cb527b7dc15cdb6f3f4ade06247d150c3bce
pnam21 <phamnamcwd@gmail.com>
2025-02-23 17:22:14 +0700

    Initial commit

---
+++ Assets/Script/Drive.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Drive : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D frontTireRB;
+    [SerializeField] private Rigidbody2D backTireRB;
+    [SerializeField] private Rigidbody2D carRB;
+    [SerializeField] private float speed = 150f;
+    [SerializeField] private float rotationSpeed = 300f;
+
+    private float moveInput;
+
+    private void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        frontTireRB.AddTorque(- moveInput * speed * Time.deltaTime);
+        backTireRB.AddTorque(-moveInput * speed * Time.deltaTime);
+        carRB.AddTorque(moveInput * rotationSpeed * Time.fixedDeltaTime);
+    }
+
+}
