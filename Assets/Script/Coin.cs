864e6f9fd24715171bf9197d48fd11799089beec
quyenbaone <cauvang2293@gmail.com>
2025-03-06 11:03:56 +0700

    co1neat

---
+++ Assets/Script/Coin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int scoreValue = 10; // Số điểm khi thu thập xu
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Vehicle")) // Kiểm tra nếu xe chạm vào xu
+        {
+            GameManager.instance.AddScore(scoreValue); // Cộng điểm
+            Destroy(gameObject); // Xóa đồng xu khỏi game
+        }
+    }
+}
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,25 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.U2D;
+using UnityEngine.UI; // Add this line
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
+    public TMP_Text scoreText; // Kéo UI Text vào đây
+    private int score = 0;
+    [SerializeField] private int price = 10;
     [SerializeField] private GameObject gameovercanvas;
-
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+        //    DontDestroyOnLoad(gameObject);
         }
+        //else
+        //{
+        //    Destroy(gameObject);
+        //}
         Time.timeScale = 1.0f;
 
     }
-
+    public void AddScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score: " + score;
+    }
     public void GameOver()
     {
         gameovercanvas.SetActive(true);
