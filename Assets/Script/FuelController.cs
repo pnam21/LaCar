eb6ee39d176d9913ddc223fb0fd5b3fd8f49858e
pnam21 <phamnamcwd@gmail.com>
2025-03-08 11:46:28 +0700

    fuel done

---
+++ Assets/Script/FuelController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public class FuelController : MonoBehaviour
+{
+    public static FuelController instance;
+    [SerializeField] private Image fuelImage;
+    [SerializeField, Range(0.1f,5f)] private float fuelDrainSpeed = 1f;
+    [SerializeField] private float maxFuelAmmount = 100f;
+    [SerializeField] private Gradient fuelGradient;
+    private float currentFuelAmmount;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    private void Start()
+    {
+        currentFuelAmmount = maxFuelAmmount;
+        UpdateUI();
+    }
+    public void Update()
+    {
+        currentFuelAmmount -= Time.deltaTime * fuelDrainSpeed;
+        UpdateUI();
+        if (currentFuelAmmount <= 0f) {
+            GameManager.instance.GameOver();
+        }
+    }
+    private void UpdateUI()
+    {
+        fuelImage.fillAmount = (currentFuelAmmount / maxFuelAmmount);
+        fuelImage.color = fuelGradient.Evaluate(fuelImage.fillAmount);
+    }
+    public void FillFuel()
+    {
+        currentFuelAmmount = maxFuelAmmount;
+        UpdateUI();
+    }
+}
