4fa2d7de26a056e4fd0f45f563ddc77d98a5e2c1
pnam21 <phamnamcwd@gmail.com>
2025-03-08 12:04:49 +0700

    fix fuel places

--- Assets/Script/FinishPoint.cs
+++ Assets/Script/FinishPoint.cs
@@ -7,20 +7,6 @@ public class FinishPoint : MonoBehaviour
 {
     [SerializeField] bool goNextLevel;
     [SerializeField] string levelName;
-    //public string Level;
-    //public void LoadLevel()
-    //{
-    //    SceneManager.LoadScene(Level);
-
-    //}
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.CompareTag("Vehicle"))
-    //    {
-    //        LoadLevel();
-    //    }
-    //}
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Vehicle"))
